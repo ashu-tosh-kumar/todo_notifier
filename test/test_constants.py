77059319af7cb3780b632336069f3dbedde0d60a
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2022-05-22 02:03:37 +0530

    Add unittest cases for constants.py

---
+++ test/test_constants.py
@@ -0,0 +1,34 @@
+import unittest
+
+from constants import (DEFAULT_EXCLUDE_DIRS, DEFAULT_EXCLUDE_FILES,
+                       DEFAULT_SUMMARY_GENERATORS_ENUM, UNKNOWN_USER_NAME)
+
+
+class TestDefaultExcludeDirs(unittest.TestCase):
+    def test_DEFAULT_EXCLUDE_DIRS_should_exist(self):
+        self.assertIsNotNone(DEFAULT_EXCLUDE_DIRS)
+
+
+class TestDefaultExcludeFiles(unittest.TestCase):
+    def test_DEFAULT_EXCLUDE_FILES_should_exist(self):
+        self.assertIsNotNone(DEFAULT_EXCLUDE_FILES)
+
+
+class TestUnknownUserName(unittest.TestCase):
+    def test_UNKNOWN_USER_NAME_should_exist(self):
+        self.assertIsNotNone(UNKNOWN_USER_NAME)
+
+
+class TestDefaultSummaryGeneratorsEnum(unittest.TestCase):
+    def test_DEFAULT_SUMMARY_GENERATORS_ENUM_should_contain_expected_enums(self):
+        self.assertEqual("Expired TODO Items",
+                         DEFAULT_SUMMARY_GENERATORS_ENUM.EXPIRED_TODO_BY_USER)
+        self.assertEqual("Module-wise Summary",
+                         DEFAULT_SUMMARY_GENERATORS_ENUM.TODO_BY_MODULE)
+        self.assertEqual("Upcoming Week TODO Items",
+                         DEFAULT_SUMMARY_GENERATORS_ENUM.UPCOMING_TODO_BY_USER)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
