cd541531255418dd1f786e5434dcad2eaa9f3a98
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2022-05-21 16:28:33 +0530

    Write core application logic - first iteration. All exceptions in exceptions.py, entry point of code in driver.py, logic to find and create TODO objects in todo_notifier.py, and all utility and helper functions in utils.py

--- driver.py
+++ driver.py
@@ -1,24 +1,25 @@
 import os
 
+from config import BaseConfig, default_config
 from notifications import send_notifications
 from todo_notifier import parse_files_for_todo_items
 from utils import get_files_in_dir
 
 
-def run(parent_dir_name: str, exclude_subdirs: list = [], exclude_files: list = []):
+def run(parent_dir_name: str, config: BaseConfig = default_config) -> None:
     """ Main run method that would get triggered to generate summary and alerts
 
     Args:
         parent_dir_name (str): Parent directory of the project
-        exclude_subdirs (list, optional): Sub directoris of `parent_dir_name` that shouldn't be considered
-        exclude_files (list, optional): Files in directory `parent_dir_name` or its sub-directories that shouldn't be considered
+        config (BaseConfig, optional): Configuration to be used. Defaults to `default_config`
     """
     curr_dir = os.getcwd()
     parent_dir = os.path.join(parent_dir_name, curr_dir)
 
     all_files_in_parent_dir = get_files_in_dir(
-        parent_dir, "py", exclude_subdirs, exclude_files)
+        parent_dir, "py", config.EXCLUDE_DIRS, config.EXCLUDE_FILES)
 
-    todo_items = parse_files_for_todo_items(all_files_in_parent_dir)
+    todo_items = parse_files_for_todo_items(
+        parent_dir_name, all_files_in_parent_dir)
 
     send_notifications(todo_items)
