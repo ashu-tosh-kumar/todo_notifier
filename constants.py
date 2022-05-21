562005382ba9378e69f93898f85818d227a29280
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2022-05-21 16:25:02 +0530

    Define models and constants

--- constants.py
+++ constants.py
@@ -1,7 +1,15 @@
-DEFAULLT_EXCLUDE_DIRS = [
-    ".*"
-]  # Default folders to exclude
+DEFAULT_EXCLUDE_DIRS = {
+    "PATTERN": [
+        ".*"
+    ],
+    "NAME": [],
+    "ABS_PATH": []
+}
 
-DEFAULLT_EXCLUDE_FILES = [
-    ".*"
-]  # Default files to exclude
+DEFAULT_EXCLUDE_FILES = {
+    "PATTERN": [
+        ".*"
+    ],
+    "NAME": [],
+    "ABS_PATH": []
+}
--- models.py
+++ models.py
@@ -1,5 +1,10 @@
-# Standard format for todo item is as follows. Note that todo has to be in capital letters
-# TODO [YYYY-MM-DD] @user_name inline_msg
+# Standard format for todo item is as follows.
+# Rules:
+# 1. todo has to be in capital letters
+# Format: TODO [YYYY-MM-DD] @user_name inline_msg
+# Date format must be YYYY-MM-DD
+# Only one todo item is allowed per line. If more than one is in one line, the first one is considered as todo item and the rest are considered as message
+# No. of spaces between todo, date, user, message is NOT important
 
 from datetime import datetime
 
@@ -7,30 +12,82 @@ from exceptions import InvalidDateFormatException
 
 
 class USER:
-    def __init__(self, user_name: str, user_email_id: str = "") -> None:
+    def __init__(self, user_name: str) -> None:
+        """ Initializer for class `USER`
+
+        Args:
+            user_name (str): User name
+        """
         self._user_name = user_name
-        self._user_email_id = user_email_id
+        self._user_email_id = None
 
     @property
-    def user_name(self):
+    def user_name(self) -> str:
+        """ Getter for `user_name`
+
+        Returns:
+            str: Returns username
+        """
         return self._user_name
 
     @property
-    def user_email_id(self):
+    def user_email_id(self) -> str:
+        """ Getter for `user_email_id`
+
+        Returns:
+            str: Returns user email id
+        """
         return self._user_email_id
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """ Defines str representation of `USER` class object
+
+        Returns:
+            str: Returns string representation of the class object
+        """
         return f"{str(self)}:{self._user_name}"
 
 
+class POSITION:
+    def __init__(self, line: int, pos: int) -> None:
+        """ Initializer for position of a text in file
+
+        Args:
+            line (int): Shows line no. of the text
+            pos (int): Shows position of the start of the text
+        """
+        self._line = line
+        self._pos = pos
+
+    @property
+    def line(self) -> int:
+        """ Getter for `line`
+
+        Returns:
+            int: Integer representing the line no. in respective module
+        """
+        return self._line
+
+    @property
+    def pos(self) -> int:
+        """ Getter for `pos`
+
+        Returns:
+            int: Integer representing start position of respective piece of code in respective module
+        """
+        return self._pos
+
+
 class TODO:
-    def __init__(self, msg: str, user: USER, completion_date_str: str) -> None:
+    def __init__(self, msg: str, user: USER, completion_date_str: str, module: str, position: POSITION) -> None:
         """ Initializer for `todo.upper()` class
 
         Args:
             msg (str): Inline message in the todo
             user (USER): User in the todo item
             completion_date_str (str): Date by which the respective `todo` item is supposed to be completed
+            module (str): Module in which todo item is present
+            position (POSITION): Represents the position of the respective todo
 
         Raises:
             InvalidDateFormatException: Raised if the `completion_date_str` is not valid or doesn't conform to expected format of "YYYY-MM-DD"
@@ -43,18 +100,58 @@ class TODO:
         except ValueError:
             raise InvalidDateFormatException(
                 f"Date: {completion_date_str} is invalid/non-supported format. Expected format: 'YYYY-MM-DD'")
+        self._module = module
+        self._position = position
 
     @property
     def msg(self) -> str:
+        """ Getter for `msg`
+
+        Returns:
+            str: Message of todo item
+        """
         return self._msg
 
     @property
-    def user(self) -> str:
+    def user(self) -> USER:
+        """ Getter for `user`
+
+        Returns:
+            USER: user object of the respective todo item
+        """
         return self._user
 
     @property
     def completion_date(self) -> datetime:
+        """ Getter for `datetime` representation of `completion_date_str`
+
+        Returns:
+            datetime: datetime object representing `completion_date_str`
+        """
         return self._completion_date
 
-    def __str__(self):
+    @property
+    def module(self) -> str:
+        """ Getter for `module`
+
+        Returns:
+            str: Module name/address of the respective todo item
+        """
+        return self._module
+
+    @property
+    def position(self) -> POSITION:
+        """ Getter for `position`
+
+        Returns:
+            POSITION: Position of respective todo item in code
+        """
+        return self._position
+
+    def __str__(self) -> str:
+        """ str representation of `todo` object
+
+        Returns:
+            str: String representation of the respective todo object
+        """
         return f"TODO: {self.msg} ASSIGNED TO: {self.user.user_name} COMPLETE BY {self.completion_date}"
