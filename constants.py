bbf54f2335c4c7d4109fe2f671e1d7173461e478
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2022-05-22 02:03:07 +0530

    Move DEFAULT_SUMMARY_GENERATORS from constants.py to summary_generators.py because of circular imports

--- constants.py
+++ constants.py
@@ -4,9 +4,21 @@ from summary_generators import (
     UpcomingWeekTodosByUser,
 )
 
-DEFAULT_EXCLUDE_DIRS = {"PATTERN": [".*"], "NAME": [], "ABS_PATH": []}
+DEFAULT_EXCLUDE_DIRS = {
+    "PATTERN": [
+        ".*"
+    ],
+    "NAME": [],
+    "ABS_PATH": []
+}
 
-DEFAULT_EXCLUDE_FILES = {"PATTERN": [".*"], "NAME": [], "ABS_PATH": []}
+DEFAULT_EXCLUDE_FILES = {
+    "PATTERN": [
+        ".*"
+    ],
+    "NAME": [],
+    "ABS_PATH": []
+}
 
 UNKNOWN_USER_NAME = "JANE_DOE"
 
