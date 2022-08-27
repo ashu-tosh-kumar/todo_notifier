e66e51082b3f6d5e53a4a7c5f3fb8ab33ea9cb15
Ashutosh Kumar <ashutosh.npr@gmail.com>
2022-08-27 20:38:02 +0530

    Remove user_code folder from repo

--- user_code/__init__.py
+++
@@ -1,3 +0,0 @@
-# Users can out all their custom code in the folder `user_code` to integrate the todo notifier in  manner they wish to
-# This folder will never be used/added to git and so it would be safe for people to git pull on todo notifier to fetch
-# latest code
