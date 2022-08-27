e54efeac1950f26527c2abfb29e3b744f9b83c9f
Ashutosh Kumar <ashutosh.npr@gmail.com>
2022-08-27 13:01:10 +0530

    Add user_code folder as a placeholder for user custom code

---
+++ user_code/__init__.py
@@ -0,0 +1,3 @@
+# Users can out all their custom code in the folder `user_code` to integrate the todo notifier in  manner they wish to
+# This folder will never be used/added to git and so it would be safe for people to git pull on todo notifier to fetch
+# latest code
