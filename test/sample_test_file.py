8e86e0cf8288dcaa391826f457db4993cd6fd2b6
Ashutosh Kumar <ashutosh.npr@gmail.com>
2022-09-03 16:18:37 +0530

    Update sample_test_file to have a standard way of writing a message

--- test/sample_test_file.py
+++ test/sample_test_file.py
@@ -1,15 +1,16 @@
 # This is a sample test file
+# To add more todo here for testing, please follow the pattern: {message}-{uuid} to add a message
 
-# TODO some message1
-# some message2 TODO some other message3
-# TODO [2022-05-01] some message 4
-# some message 5 TODO [2022-05-02] some message 6
-# TODO [2022-05-03] @ashutosh some message 7
-# some message 8 TODO [2022-05-04] @ashutosh some message 8
+# TODO some-message-21f886ac-cc41-452b-9a53-3cfd56446341
+# some-message-e8ce523f-4d4b-48e0-abbc-54dee9cb2706 TODO some-message-0e4fc40f-f7c6-40bb-a6db-a51e158f4cc4
+# TODO [2022-05-01] some-message-0b0e31cc-e2e8-4d54-9684-ddf95958d7dc
+# some-message-c1db6487-85a1-42c2-844b-1b40f8c602a9' TODO [2022-05-02] some-message-f53d566a-1fea-422b-9ac6-41a171977ac2
+# TODO [2022-05-03] @ashutosh some-message-4ef1fe34-ab50-4cc1-8abc-e550dee3be3f
+# some-message-cb7a4389-c880-4115-9ff9-373363e5ecb1 TODO [2022-05-04] @ashutosh some-message-2af38dcf-29e4-4104-9921-6e654f27a6ca
 
 
 class SampleClass:
-    """TODO [2022-05-05] @ashutosh some message 9"""
+    """TODO [2022-05-05] @ashutosh some-message-a1571791-50fe-4863-8a06-c2d4d921897c"""
 
     pass
 
@@ -18,21 +19,21 @@ class TODO:
     pass
 
 
-""" TODO [2022-05-06] @ashutosh some message 10 """
+""" TODO [2022-05-06] @ashutosh some-message-ffe6e2ca-c78b-4231-b278-06e3de8aa9c6"""
 
 """
-TODO [2022-05-07] @ashutosh some message 11
-in other line message
+TODO [2022-05-07] @ashutosh some-message-a59653a5-3fae-450a-8289-0c4433e27535
+some-message-in-another-line-6e7b3874-9e96-4596-8453-29bec81536ef
 """
 
-# TODO @ashutosh some message 12
-# TODO [2022-05-08] some message 13
+# TODO @ashutosh some-message-6d3d70fd-0dfc-46d6-a1ab-2303c292701c
+# TODO [2022-05-08] some-message-5c795770-18f7-4104-a5d5-908af39a6686
 # TODO [2022-05-09] @ashutosh
 # TODO [2022-05-10]
 # TODO @ashutosh
 # TODO
 
 # TODO TODO
-# TODO @ashutosh TODO [2022-05-11] some message 14
-# TODO @ashutosh # TODO [2022-05-12] some message 15
-# TODO [random message 1] @ashutosh
+# TODO @ashutosh TODO [2022-05-11] some-message-7c2a62cb-8037-46cb-bdee-76dd0791d747
+# TODO @ashutosh # TODO [2022-05-12] some-message-d2cad1b4-8881-4687-a38d-2d14fbfd1306
+# TODO [random-message-96a9cc8e-3bab-40d3-a2d2-735c267e1c65] @ashutosh
