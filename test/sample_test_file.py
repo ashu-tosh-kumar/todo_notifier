c9970c73922e7ed2db456b5bb0fcb8ed1938e65d
Ashutosh Kumar <ashutosh.npr@gmail.com>
2022-09-04 09:33:19 +0530

    Add more test cases to sample_test_file.py

--- test/sample_test_file.py
+++ test/sample_test_file.py
@@ -7,6 +7,35 @@
 # some-message-c1db6487-85a1-42c2-844b-1b40f8c602a9' TODO [2022-05-02] some-message-f53d566a-1fea-422b-9ac6-41a171977ac2
 # TODO [2022-05-03] @ashutosh some-message-4ef1fe34-ab50-4cc1-8abc-e550dee3be3f
 # some-message-cb7a4389-c880-4115-9ff9-373363e5ecb1 TODO [2022-05-04] @ashutosh some-message-2af38dcf-29e4-4104-9921-6e654f27a6ca
+# TODO some-message-f264a5e7-5758-4145-9513-c5c52c20a9fa TODO some-message-43498a94-6b13-41e8-ac2e-05e84fc6c895
+# TODO [2022-05-03 @ashutosh some-message-faa74c20-d4c2-444e-9fa0-7f202cb10b44
+# TODO 2022-05-03] @ashutosh some-message-63448d77-07fb-4bc1-bf80-2d94ab902f5f
+# TODO 2022-05-03 @ashutosh some-message-11c6c4a1-5231-47ec-8cc6-11de055ce441
+# TODO [2022-05] @ashutosh some-message-e1922c58-f6d0-49cf-8005-076c18556f13
+# TODO [2022-13] @ashutosh some-message-1f1d31c2-2a40-4d02-9bcd-b099de963450
+# TODO [05-03] @ashutosh some-message-e8d9c141-f082-44a3-b828-6f83606e9d24
+# TODO [2022] @ashutosh some-message-1a4a2281-40b0-4df0-942e-8d1a9d74ad45
+# TODO [03] @ashutosh some-message-c3757bc8-571f-4717-bf16-b102a16d36f5
+# TODO [13] @ashutosh some-message-8882c77a-ba29-40dc-9bcd-fb7d19ce7d49
+# TODO @ashutosh some-message-6d3d70fd-0dfc-46d6-a1ab-2303c292701c
+# TODO [2022-05-08] some-message-5c795770-18f7-4104-a5d5-908af39a6686
+# TODO [2022-05-09] @ashutosh
+# TODO [2022-05-09] some-@message-726c9e2d-13bf-4309-9b3c-530c7b3404b7
+# TODO some-message-27cdb8bc-fffe-40df-a212-76452cbdb47c [2022-05-09] some-@message-db1d606f-88d3-474e-890e-8e9c34f093d0
+# TODO [2022-05-09]
+# TODO [2022-05-10]
+# TODO @ashutosh
+# TODO ashu@tosh
+# TODO @ashutosh some-message-35d43513-00df-4c3d-8569-b72d8983f62d
+# TODO
+# TODO some-message-1621700c-bf13-49b7-ab63-e544d570f341
+# TODO @ashutosh [2022-05-09]
+# TODO some-message-40b01cd7-3480-4f9b-9d0c-98ad297cf591 @ashutosh [2022-05-09]
+# TODO @ashutosh [2022-05-09] some-message-afc38fb9-5a40-470b-9e1c-d4bacc451727
+# TODO TODO
+# TODO @ashutosh TODO [2022-05-11] some-message-7c2a62cb-8037-46cb-bdee-76dd0791d747
+# TODO @ashutosh # TODO [2022-05-12] some-message-d2cad1b4-8881-4687-a38d-2d14fbfd1306
+# TODO [random-message-96a9cc8e-3bab-40d3-a2d2-735c267e1c65] @ashutosh
 
 
 class SampleClass:
@@ -25,15 +54,3 @@ class TODO:
 TODO [2022-05-07] @ashutosh some-message-a59653a5-3fae-450a-8289-0c4433e27535
 some-message-in-another-line-6e7b3874-9e96-4596-8453-29bec81536ef
 """
-
-# TODO @ashutosh some-message-6d3d70fd-0dfc-46d6-a1ab-2303c292701c
-# TODO [2022-05-08] some-message-5c795770-18f7-4104-a5d5-908af39a6686
-# TODO [2022-05-09] @ashutosh
-# TODO [2022-05-10]
-# TODO @ashutosh
-# TODO
-
-# TODO TODO
-# TODO @ashutosh TODO [2022-05-11] some-message-7c2a62cb-8037-46cb-bdee-76dd0791d747
-# TODO @ashutosh # TODO [2022-05-12] some-message-d2cad1b4-8881-4687-a38d-2d14fbfd1306
-# TODO [random-message-96a9cc8e-3bab-40d3-a2d2-735c267e1c65] @ashutosh
