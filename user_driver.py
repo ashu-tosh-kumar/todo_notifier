1ef720a43aae2654d41dd3348763058da67c48c7
Ashutosh Kumar <ashutosh.npr@gmail.com>
2022-10-22 12:00:08 +0530

    Update user_driver.py wrt latest changes in code

--- user_driver.py
+++ user_driver.py
@@ -1,20 +1,22 @@
 from config import BaseConfig, DefaultConfig
-from connect import CONNECT_METHOD
+from connect import CONNECT_METHOD, Connect
 from driver import run as driver_run
 
 
 def run():
     """Main starting point of the TODO application to be modified by end users per their need
 
+    This script can be modified to add a for loop for multiple projects
+
     This file/function after initial code commit should be added in `.gitignore` so that users
     can safely update the TODO application by simply pulling the latest changes
     """
-    git_url: str = ""  # Placeholder for HTTPS based git url
-    project_dir_name: str = ""  # Placeholder. Must match project name in git repository
-    config: BaseConfig = DefaultConfig(connect_method=CONNECT_METHOD.HTTPS, save_html_reports=False)  # Change per need
-    connect_kwargs = dict(git_url=git_url, project_dir_name=project_dir_name)
+    git_url: str = ""  # Placeholder for HTTPS/SSH based git url
+    project_dir_name: str = ""  # Placeholder. Must match project name in git repository.
+    config: BaseConfig = DefaultConfig(save_html_reports=True, ignore_todo_case=True)  # Change per need
+    connect = Connect(connect_method=CONNECT_METHOD.GIT_CLONE, project_dir_name=project_dir_name, url=git_url)
 
-    driver_run(connect_kwargs=connect_kwargs, config=config)
+    driver_run(connect=connect, config=config)
 
 
 if __name__ == "__main__":
