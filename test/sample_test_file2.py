c7ddc2ff8c85a0fdf75d46f3463631a2292b0972
Ashutosh Kumar <ashutosh.npr@gmail.com>
2022-10-22 08:59:06 +0530

    Increase test coverage

---
+++ test/sample_test_file2.py
@@ -0,0 +1,5 @@
+# This is a sample test file
+# To add more todo here for testing, please follow the pattern: {message}-{uuid} to add a message
+
+# TODO some-message-21f886ac-cc41-452b-9a53-3cfd56446341
+# some-message-e8ce523f-4d4b-48e0-abbc-54dee9cb2706 TODO some-message-0e4fc40f-f7c6-40bb-a6db-a51e158f4cc4
