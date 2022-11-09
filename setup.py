98b9578c0e4243bf151ba289b82fc8e37e3899f1
Ashutosh Kumar <ashutosh.npr@gmail.com>
2022-11-10 03:57:23 +0530

    Skip test folder in package creation

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     test_suite="unittest",
     version="1.1.0",
     keywords=["todo", "notifier"],
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(exclude=["test"]),
     install_requires=[
         "GitPython==3.1.29",
         "python-dateutil==2.8.2",
