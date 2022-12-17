223646625771b7be4d021d6312aa7120514a6aff
Ashutosh Kumar <ashutosh.npr@gmail.com>
2022-12-17 16:37:34 +0530

    bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     download_url="",  # To add later
     license="MIT",
     test_suite="unittest",
-    version="1.2.0",
+    version="1.2.1",
     keywords=["todo", "notifier"],
     packages=find_packages(exclude=["tests", "sample_reports"]),
     install_requires=[
